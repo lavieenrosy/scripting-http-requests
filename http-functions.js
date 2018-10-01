f88c884f183f3014b37f2b8e5993a2920da48294
Rosy Lee <rosyhlee@gmail.com>
2018-10-01 20:52:42 +0000

    adding http-functions and test.js files

---
+++ http-functions.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+
+  var chunksOfData = "";
+
+  https.get(options, function(response) {
+    response.setEncoding('utf8');
+    response.on('data', function(data) {
+      chunksOfData += data + '\n';
+      // console.log(chunksOfData);
+    });
+    //invoke callback when the data is fully received
+    response.on('end', function() {
+      return callback(chunksOfData);
+    });
+  });
+};
+
+function printHTML(html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step3.html'
+};
+
+getHTML(requestOptions, printHTML);
+
+module.exports = {
+  getHTML: getHTML,
+}
